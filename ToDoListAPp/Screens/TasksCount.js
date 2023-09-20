51f477a0cfa43973e41282cb601456c77f815949
fahd-111 <fahad.jcat123@gmail.com>
2023-09-20 17:48:59 +0500

    h

--- ToDoListAPp/Screens/TasksCount.js
+++ ToDoListAPp/Screens/TasksCount.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
 import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
